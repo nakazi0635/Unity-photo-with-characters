5c33723dde3046e9cc56bdb3d99bd1c77b34d1bf
nakazi <nakazi1209@gmail.com>
2023-06-03 11:45:57 +0900

    add csfile

---
+++ Assets/Scripts/ChengeButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChengeButton : MonoBehaviour
+{
+    [SerializeField] Animator anim; // アニメーターを操作するための変数
+    [HideInInspector] int posemode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posemode = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnClick() // ボタンを押したときの処理
+    {
+        if (posemode == 0){
+            anim.SetInteger("PoseChange", 1); // アニメーターのPoseChangeを1にする
+            posemode = 1;
+        }else{
+            anim.SetInteger("PoseChange", 0);
+            posemode = 0;
+        }
+    }
+}
