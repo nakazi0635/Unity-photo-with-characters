445333e7c8777258134b8c7f4942271e3fc9b1e5
nakazi <nakazi1209@gmail.com>
2023-06-04 01:20:36 +0900

    update csfile

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -6,67 +6,66 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     [SerializeField] 
-    Camera ArCamera; //カメラクラスの変数呼び出し
+    Camera arCamera; //カメラクラスの変数呼び出し
     [HideInInspector]
     Vector2 lastMousePosition; //直前のマウスのワールド座標
+    // [HideInInspector] 
+    // float movementSpeed = 1.5f; //横回転のスピード
     [HideInInspector] 
-    float MovementSpeed = 1.5f; //横回転のスピード
+    float rotationY; //横回転のスピード
     [HideInInspector] 
-    float angleY = 0; //横回転のスピード
+    Vector3 screenCharacterPosition; // キャラのスクリーン座標(2D)変数
+    [HideInInspector] 
+    Vector3 worldCharacterPosition; // キャラのワールド座標(3D)変数
+    [SerializeField] 
+    GameObject capsuleColliderObject;
+    [HideInInspector] 
+    CapsuleCollider capsuleCollider; // キャラのワールド座標(3D)変数
 
+    void Start()
+    {
+        capsuleCollider = capsuleColliderObject.GetComponent<CapsuleCollider>();
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0)){ // 右クリックした瞬間
+        // if (Input.GetKeyDown(KeyCode.Mouse0)){ // 右クリックした瞬間
 
-            lastMousePosition = ArCamera.WorldToScreenPoint(Input.mousePosition);//ワールド座標(3D)をスクリーン座標(2D)に変換
+        //     lastMousePosition = arCamera.WorldToScreenPoint(Input.mousePosition);//ワールド座標(3D)をスクリーン座標(2D)に変換
 
-        }else if(Input.GetKey(KeyCode.Mouse0)){ //右クリックしている間
+        // }else if(Input.GetKey(KeyCode.Mouse0)){ //右クリックしている間
+        //     if (drag == false){ // マウスでドラッグされていないなら
+        //         rotationY = lastMousePosition.x - arCamera.WorldToScreenPoint(Input.mousePosition).x;
 
-            if (transform.up.y >= 0) //もしキャラクターの向きが上なら
-            {
-                angleY = lastMousePosition.x - ArCamera.WorldToScreenPoint(Input.mousePosition).x;
-            }
-            else //もしキャラクターの向きが下なら
-            {
-                angleY = -(lastMousePosition.x - ArCamera.WorldToScreenPoint(Input.mousePosition).x);
-            }
+        //         Vector3 angle = new Vector3(0, rotationY * movementSpeed, 0); // 回転する角度を求める
 
-            //回転する角度を求める
-            Vector3 angle = new Vector3(0, angleY * MovementSpeed, 0);
-
-            //求めた角度の分だけ回転する
-            this.transform.Rotate(angle);
-            //lastMousePotionにマウスのワールド座標を代入
-            lastMousePosition = ArCamera.WorldToScreenPoint(Input.mousePosition);
-        }
+        //         this.transform.Rotate(angle); // 求めた角度の分だけ回転する
+        //     }
+        //     //lastMousePotionにマウスのワールド座標を代入
+        //     lastMousePosition = arCamera.WorldToScreenPoint(Input.mousePosition);
+        // }
     }
 
 
-    private void OnMouseDrag() //キャラクターがクリック(ドラッグ)されたとき
+    private void OnMouseDrag() // キャラクターがドラッグされたら
     {
+        screenCharacterPosition = arCamera.WorldToScreenPoint(transform.position); // キャラクターのワールド座標(3D)をスクリーン座標(2D)に変換して、screenCharacterPositionに代入
 
-        //キャラクターの位置をワールド座標からスクリーン座標に変換して、screenPlayerPointに代入
-        Vector3 screenPlayerPoint = ArCamera.WorldToScreenPoint(transform.position);
+        screenCharacterPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenCharacterPosition.z); //マウスポインタの位置を、screenCharacterPositionに代入
+        // screenCharacterPosition = new Vector3(Input.mousePosition.x + capsuleCollider.center.x, Input.mousePosition.y + capsuleCollider.center.y, screenCharacterPosition.z); //マウスポインタの位置を、screenCharacterPositionに代入
 
-        //キャラクターの現在位置(マウス位置)を、screenPlayerPointに代入
-        screenPlayerPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPlayerPoint.z);
+        worldCharacterPosition = arCamera.ScreenToWorldPoint(screenCharacterPosition); //キャラクターのスクリーン座標(2D)をワールド座標(3D)に変換して、worldCharacterPositionに代入
 
-        //キャラクターの現在位置を、スクリーン座標からワールド座標に変換して、worldPlayerPointに格納
-        Vector3 worldPlayerPoint = ArCamera.ScreenToWorldPoint(screenPlayerPoint);
-        worldPlayerPoint.z = transform.position.z;
-
-        //キャラクターの位置を、worldPlayerPointにする
-        transform.position = worldPlayerPoint;
+        transform.position = worldCharacterPosition; //キャラクターの位置を、worldCharacterPositionにする
     }
 
     public void OnPushRightRotation() //右回転関数
     {
-        transform.Rotate(0, -30, 0, Space.World);
+        transform.Rotate(0, 0, -30, Space.World);
     }
 
     public void OnPushLeftRotation() //左回転関数
     {
-        transform.Rotate(0, 30, 0, Space.World);
+        transform.Rotate(0, 0, 30, Space.World);
     }
 }
