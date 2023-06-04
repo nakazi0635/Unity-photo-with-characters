8ee758671006d060d31f34df2871c30400cfc3ad
nakazi <nakazi1209@gmail.com>
2023-06-04 23:25:16 +0900

    update Indicates non-indicative

--- Assets/Scripts/IndicatesButton.cs
+++ Assets/Scripts/IndicatesButton.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class IndicatesButton : MonoBehaviour
 {
-    [SerializeField]
-    GameObject canvasOn; // 編集キャンバス
-    [SerializeField]
-    GameObject canvasOff; // 確認キャンバス
+    public GameObject buttonOn; // 編集キャンバス
+    public GameObject buttonOff; // 確認キャンバス
     // Start is called before the first frame update
     void Start()
     {
-        canvasOff.SetActive(false); // 確認キャンバスを非表示する
+        buttonOff.SetActive(false); // 確認キャンバスを非表示する
     }
 
     // Update is called once per frame
@@ -20,12 +18,12 @@ public class IndicatesButton : MonoBehaviour
         
     }
     public void OnPushOnButton(){
-        canvasOn.SetActive(true); // 編集キャンバスを表示する
-        canvasOff.SetActive(false); // 確認キャンバスを非表示する
+        buttonOn.SetActive(true); // 編集キャンバスを表示する
+        buttonOff.SetActive(false); // 確認キャンバスを非表示する
 
     }
     public void OnPushOffButton(){
-        canvasOn.SetActive(false); // 編集キャンバスを非表示する
-        canvasOff.SetActive(true); // 確認キャンバスを表示する
+        buttonOn.SetActive(false); // 編集キャンバスを非表示する
+        buttonOff.SetActive(true); // 確認キャンバスを表示する
     }
 }
--- Assets/Scripts/ScreenShot.cs
+++ Assets/Scripts/ScreenShot.cs
@@ -6,97 +6,55 @@ using UnityEngine;
 
 public class ScreenShot : MonoBehaviour
 {
-    float countTime = 0; //時間を計測する変数
-    bool setTimer = false; //タイマー機能
 
-    [SerializeField] GameObject buttons; //撮影時に見えないようにするボタングループ
-    [SerializeField] GameObject waiting; //撮影後に表示する待機画面
 
-    // Start is called before the first frame update
+    [Header("保存先の設定")]
+    [SerializeField]
+    string folderName = "Screenshots";
+    [SerializeField]
+    IndicatesButton indicatesButtons;
+    bool isCreatingScreenShot = false;
+    string path;
+
     void Start()
     {
-        //待機画面は起動時に非表示にする
-        waiting.SetActive(false);
+        path = Application.dataPath + "/" + folderName + "/";
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PrintScreen()
     {
-        if (setTimer == true) //setTimerがオンになったら
-        {
-            countTime += Time.deltaTime; //時間を計測する
-
-            if (countTime >= 5) //5秒後に
-            {
-                countTime = 0;
-                StartCoroutine(ScreenCap());
-                setTimer = false;
-            }
-        }
+        StartCoroutine("PrintScreenInternal");
     }
 
-    private IEnumerator ScreenCap()
+    IEnumerator PrintScreenInternal()
     {
-        //ファイル名重ならないように年月日時間の情報を入れる
-        string now = System.DateTime.Now.ToString().Replace(":", "").Replace("/", "").Replace(" ", "");
-
-        string filename = now + ".png";
-
-        string imagePath = "";
-
-        //プラットフォームごとの保存先の変更処理
-        switch (Application.platform)
+        indicatesButtons.buttonOn.SetActive(false);
+        indicatesButtons.buttonOff.SetActive(false);
+        if (isCreatingScreenShot)
         {
-            case RuntimePlatform.Android:
-                //filename = Application.persistentDataPath + "/" + now + ".png";
-                imagePath = System.IO.Path.Combine(Application.persistentDataPath, filename);
-                Debug.Log("Platform is complete");
-                break;
-
-            case RuntimePlatform.IPhonePlayer:
-                //filename = Application.persistentDataPath + "/" + now + ".png";
-                imagePath = System.IO.Path.Combine(Application.persistentDataPath, filename);
-                break;
-
-            default:
-                filename = Application.dataPath + "/" + now + ".png";
-                imagePath = System.IO.Path.Combine(Application.dataPath, filename);
-                break;
+            yield break;
         }
 
+        isCreatingScreenShot = true;
 
-        //filename = Application.persistentDataPath + "/" + now + ".png";
+        yield return null;
 
-        //ボタンを非表示にする
-        buttons.SetActive(false);
-
-        //スクリーンショットを撮影する
-        ScreenCapture.CaptureScreenshot(filename);
-
-        //スクリーンショットが生成されるまで
-        while (!System.IO.File.Exists(imagePath))
+        if (!Directory.Exists(path))
         {
-            //次のフレームまで待機する
-            yield return null;
-
-            //待機画面が表示されていなければ
-            if (waiting.activeSelf == false)
-            {
-                Debug.Log("wait");
-                //待機画面を表示する
-                waiting.SetActive(true);
-            }
+            Directory.CreateDirectory(path);
         }
 
-        //スクリーンショットが生成された後
-        //ボタンを表示する
-        buttons.SetActive(true);
-        //待機画面を非表示にする
-        waiting.SetActive(false);
+        string date = DateTime.Now.ToString("yy-MM-dd_HH-mm-ss");
+        string fileName = path + date + ".png";
+
+        ScreenCapture.CaptureScreenshot(fileName);
+
+        yield return new WaitUntil(() => File.Exists(fileName));
+
+        isCreatingScreenShot = false;
+
+        indicatesButtons.buttonOn.SetActive(true);
+        indicatesButtons.buttonOff.SetActive(true);
     }
 
-    public void OnPushScreenshot() //スクリーンショットボタン
-    {
-        setTimer = true; //タイマー機能がオンになる
-    }
 }
