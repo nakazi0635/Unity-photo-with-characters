8217cf302c74a4e4df398fb52df34d5407ff4ac2
nakazi <nakazi1209@gmail.com>
2023-06-07 09:29:43 +0900

    Update csfile

--- Assets/Scripts/Lighting.cs
+++ Assets/Scripts/Lighting.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Lighting : MonoBehaviour
 {
     [SerializeField]
-    GameObject character; // 
+    GameObject character; // キャラクターのオブジェクトを代入する変数
     [SerializeField]
     GameObject parent; // 光源とキャラクターの親オブジェクトを代入する変数
     [SerializeField]
-    int nextPosition = 1; // 
+    int nextPosition = 1; // Lightのポジションを変更するための変数
     // Start is called before the first frame update
     void Start()
     {
