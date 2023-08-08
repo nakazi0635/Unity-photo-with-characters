5c6372398f307dc6fcb3c5040e331606e6607a89
nakazi <nakazi1209@gmail.com>
2023-08-08 21:23:12 +0900

    回転機能の修正。Animatorをキー操作に対応

--- Assets/Scripts/ChangePoseButton.cs
+++ Assets/Scripts/ChangePoseButton.cs
@@ -15,18 +15,22 @@ public class ChangePoseButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.UpArrow)||Input.GetKeyDown(KeyCode.RightArrow)||Input.GetKeyDown(KeyCode.LeftArrow)){
+            anime.SetInteger("PoseNum", 1);
+        }else if(Input.GetKeyUp(KeyCode.UpArrow)||Input.GetKeyUp(KeyCode.RightArrow)||Input.GetKeyUp(KeyCode.LeftArrow)){
+            anime.SetInteger("PoseNum", 0);
+        }
     }
     public void OnClick() // ボタンを押したときの処理
     {
         if (pose_mode == 0){
-            anime.SetInteger("PoseNum", 1); // アニメーターに設定したPoseNum変数を0にする
+            anime.SetInteger("PoseNum", 1); // アニメーターに設定したPoseNum変数を1にする
             pose_mode = 1; // 次のポーズ番号を設定する
         }else if(pose_mode == 1){
-            anime.SetInteger("PoseNum", 2); // アニメーターに設定したPoseNum変数を1にする
+            anime.SetInteger("PoseNum", 2); // アニメーターに設定したPoseNum変数を2にする
             pose_mode = 2; // 次のポーズ番号を設定する
         }else{
-            anime.SetInteger("PoseNum", 0); // アニメーターに設定したPoseNum変数を2にする
+            anime.SetInteger("PoseNum", 0); // アニメーターに設定したPoseNum変数を0にする
             pose_mode = 0; // 次のポーズ番号を設定する
         }
     }
