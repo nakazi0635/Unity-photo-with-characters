880cbb4b453df66d72202f478a34ee478e0477a4
nakazi <nakazi1209@gmail.com>
2023-08-14 22:36:11 +0900

    モデルアップロードの下準備

---
+++ Assets/Scripts/UploadModel.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UploadModel : MonoBehaviour
+{
+    public string path = "path/to/your/model.obj";
+}
