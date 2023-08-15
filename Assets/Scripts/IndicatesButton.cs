5d8041412d276e75f2b544eff24016101add0cff
nakazi <nakazi1209@gmail.com>
2023-08-15 22:23:34 +0900

    UI切り替え機能の完成

--- Assets/Scripts/IndicatesButton.cs
+++ Assets/Scripts/IndicatesButton.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class IndicatesButton : MonoBehaviour
 {
+    
     public GameObject buttonOn; // 編集キャンバス
+    public GameObject buttonDragMode; // ドラッグキャンバス
     public GameObject buttonOff; // 確認キャンバス
     // Start is called before the first frame update
     void Start()
     {
-        buttonOff.SetActive(false); // 確認キャンバスを非表示する
+        buttonOff.SetActive(false); // 確認キャンバスを非表示
     }
 
     // Update is called once per frame
@@ -18,12 +20,19 @@ public class IndicatesButton : MonoBehaviour
         
     }
     public void OnPushOnButton(){
-        buttonOn.SetActive(true); // 編集キャンバスを表示する
-        buttonOff.SetActive(false); // 確認キャンバスを非表示する
+        buttonOn.SetActive(true); // 編集キャンバスを表示
+        buttonOff.SetActive(false); // 確認キャンバスを非表示
+        buttonDragMode.SetActive(false); // ドラッグキャンバスを非表示
 
     }
     public void OnPushOffButton(){
-        buttonOn.SetActive(false); // 編集キャンバスを非表示する
-        buttonOff.SetActive(true); // 確認キャンバスを表示する
+        buttonOn.SetActive(false); // 編集キャンバスを非表示
+        buttonOff.SetActive(true); // 確認キャンバスを表示
+        buttonDragMode.SetActive(false); // ドラッグキャンバスを表示
+    }
+    public void OnPushDragButton(){
+        buttonOn.SetActive(false); // 編集キャンバスを非表示
+        buttonOff.SetActive(false); // 確認キャンバスを表示
+        buttonDragMode.SetActive(true); // ドラッグキャンバスを表示
     }
 }
